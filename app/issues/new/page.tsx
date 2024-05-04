3af3d6a7fc6fe967c48e2a42354f906f3a79ff58
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-05-04 17:16:52 +0600

    add a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import "easymde/dist/easymde.min.css";
 import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod'
 import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
@@ -19,6 +20,7 @@ const NewIssuePage = () => {
         resolver: zodResolver(createIssueSchema)
     });
     const [error, setError] = useState('')
+    const [isSubmitting, setIsSubmitting] = useState(false)
 
     return (
         <div className='max-w-xl '>
@@ -30,9 +32,11 @@ const NewIssuePage = () => {
                 className='space-y-3'
                 onSubmit={handleSubmit(async (data) => {
                     try {
+                        setIsSubmitting(true);
                         await axios.post('/api/issues', data);
                         router.push('/issues');
                     } catch (error) {
+                        setIsSubmitting(false)
                         setError('An unexpected error occurred.');
                     }
                 })}>
@@ -54,7 +58,12 @@ const NewIssuePage = () => {
                 <ErrorMessage>
                     {errors.description?.message}
                 </ErrorMessage>
-                <Button>Submit New Issue</Button>
+                <Button disabled={isSubmitting}>
+                    Submit New Issue
+                    {isSubmitting &&
+                        <Spinner />
+                    }
+                </Button>
             </form>
         </div>
     )
